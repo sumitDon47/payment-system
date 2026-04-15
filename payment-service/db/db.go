8f477358a07d210047e596808671a79dbce75f16
Sumit Sapkota <139694105+sumitDon47@users.noreply.github.com>
2026-04-15 09:23:34 +0545

    modify db.go

--- payment-service/db/db.go
+++ payment-service/db/db.go
@@ -13,12 +13,12 @@ var DB *sql.DB
 
 func Connect() {
 	connStr := fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname-%s sslmode-disable",
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_PORT"),
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_DBNAME"),
+		os.Getenv("DB_NAME"),
 	)
 
 	var err error
